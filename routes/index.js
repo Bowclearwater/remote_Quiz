0e60f45d360ea9042c66cd237b9140c31233a8bc
Bow Clearwater <rasamalagon@gmail.com>
2015-07-21 10:29:08 +0200

    Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,16 @@ var router = express.Router();
 
 var quizController=require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Página de entrada (home page)
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+res.render('index', { title: 'Quiz' });
 });
+// Definición de rutas de /quizes
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+module.exports = router;
 
 router.get('/author',function(req,res){
-	res.render('author',{title:'Autores'});
+	res.render('author',{ title: 'Creditos'});
 });
-
-router.get('/quizes/question',quizController.question);
-router.get('/quizes/answer',quizController.answer);
-
-
-module.exports = router;
