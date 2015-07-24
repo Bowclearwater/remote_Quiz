04f8281de5f5413592679edcb6971826249260ec
Bow Clearwater <rasamalagon@gmail.com>
2015-07-24 20:16:55 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -34,8 +34,6 @@ exports.index = function(req, res) {
 						});
 	}
 
-
-
 // GET /quizes/:id
 exports.show = function(req, res) {
 res.render('quizes/show', { quiz: req.quiz});
@@ -47,4 +45,21 @@ if (req.query.respuesta === req.quiz.respuesta) {
 resultado = 'Correcto';
 }
 res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
+
+//GET /quizes/new
+exports.new=function(req,res){
+	var quiz=models.Quiz.build(
+		{pregunta:"Pregunta",respuesta:"Respuesta"}
+		);
+	res.render('quizes/new',{quiz:quiz}); 
+}
+
+//POST /quizes/create
+exports.create=function(req,res){
+	var quiz=models.Quiz.build(req.body.quiz); //post envia en el cuerpo del html
+	//guarda en DB  los campos pregunta y respuesta  de quiz
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	})	//Redireccion HTTP (Url relativo) lista preguntas
 };
